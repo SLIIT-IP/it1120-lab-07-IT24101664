9ce736e5f585116e285e6d14a9b0484ec4fdd0b2
IT24101664 <it24101664@my.sliit.lk>
2024-09-30 20:12:03 +0530

    Add files via upload

---
+++ Q2/IT24101664Lab7Q2B.java
@@ -0,0 +1,13 @@
+public class IT24101664Lab7Q2B{
+ public static void main(String[] args){
+    int i,j=1;
+    for(i = 1; i <= 5; i++){
+        System.out.print(+i+ " -");
+    
+    for(j = 1; j <= i; j++){
+            System.out.print(" *");
+        }
+        System.out.println();
+    }
+ }
+}
